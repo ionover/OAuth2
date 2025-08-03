34258e032660deea8b8f3d784d4f7cd3f7610534
v.ionov <v.ionov@mycrg.ru>
2025-08-03 16:15:20 +0300

    init

--- src/main/java/org/example/oauth2/WebSecurityConfigurerAdapterImpl.java
+++ src/main/java/org/example/oauth2/WebSecurityConfigurerAdapterImpl.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,11 +26,10 @@ public class WebSecurityConfigurerAdapterImpl {
                     .requestMatchers("/secure/**").authenticated()
                     .anyRequest().authenticated()
             )
-            .formLogin(form -> form
-                    .permitAll()
+            .formLogin(AbstractAuthenticationFilterConfigurer::permitAll
             )
-            .logout(logout -> logout.permitAll())
-            .csrf(csrf -> csrf.disable());
+            .logout(LogoutConfigurer::permitAll)
+            .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
